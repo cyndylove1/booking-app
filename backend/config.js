986cdf7cf45f561f1569365b1a7edf4ee5ce942b
cynthia <nwekecynthia2015@gmail.com>
2023-09-26 14:58:00 -0700

    intial commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://cyndylove:08158802091@cluster0.is7jbgy.mongodb.net/cluster0?retryWrites=true&w=majority'
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
